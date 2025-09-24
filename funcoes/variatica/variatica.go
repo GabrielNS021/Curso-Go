1bd667fe7f7831869411c780ecea979d4ccf61bd
Gabriel <112496072+GabrielNS021@users.noreply.github.com>
2025-09-24 19:47:46 +0000

    24/09

---
+++ funcoes/variatica/variatica.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func media(numeros ... float64) float64 {
+	total := 0.0
+	for _, num := range numeros {
+		total += num
+	}
+	return total / float64(len(numeros))
+}
+
+func main() {
+	fmt.Printf("Media: %.2f\n", media(1.0, 9.9, 7.2))
+}
